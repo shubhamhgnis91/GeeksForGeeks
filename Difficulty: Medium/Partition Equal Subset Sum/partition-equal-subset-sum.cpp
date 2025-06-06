fab36771236b7b1475c746a79bcc3dabbf552075
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2025-06-06 22:41:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+  
+    bool equalPartition(vector<int>& arr) {
+        // code here
+        
+        int n = arr.size();
+        
+        int sum = 0;
+        for(auto i:arr){
+            sum += i;
+        }
+        
+        if(sum%2!=0)
+            return false;
+            
+        sum/=2;
+        vector<vector<bool>> dp(n+1, vector<bool>(sum + 1, false));
+        
+        for(int i=0; i<= n; i++){
+            dp[i][0] = true;
+        }
+        
+        for(int i = 1; i<= n; i ++){
+            for(int j = 1; j<= sum; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = (dp[i - 1][j - arr[i - 1]]) || (dp[i - 1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
+        
+        
+    }
+};
